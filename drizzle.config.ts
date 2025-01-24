faf6e998b7e8c7208169560dd9881e4b6409f382
Pickle58 <jpilkington332@gmail.com>
2025-01-24 06:22:02 -0700

    added dependencies

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "drizzle-kit";
+
+import * as dotenv from "dotenv";
+
+dotenv.config({
+    path: '.env',
+    out: './src/db/migrations'
+    dbConnections: {
+        url:
+    }
+})
